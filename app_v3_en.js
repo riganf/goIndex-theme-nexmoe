7cf5e321a9d1d24578583af0b0c46b444036f8c1
Genos2000 <paraskamble2000@gmail.com>
2020-10-01 15:34:28 +0530

    try async loading

--- app_v3_en.js
+++ app_v3_en.js
@@ -5479,34 +5479,34 @@
   );
 });
 document.write(
-  '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/xd003/goIndex-theme-nexmoe@master/css/mdui_v2.min.css">'
+  '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/xd003/goIndex-theme-nexmoe@master/css/mdui_v2.min.css" asyc>'
 );
 document.write(
-  '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/xd003/goIndex-theme-nexmoe@master/css/nexmoe_v2.min.css">'
+  '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/xd003/goIndex-theme-nexmoe@master/css/nexmoe_v2.min.css" async>'
 );
 document.write(
-  '<script src="//cdn.jsdelivr.net/gh/xd003/goIndex-theme-nexmoe@master/js/mdui.min.js"></script>'
+  '<script src="//cdn.jsdelivr.net/gh/xd003/goIndex-theme-nexmoe@master/js/mdui.min.js"></script async>'
 );
 document.write(
-  '<script src="//cdn.jsdelivr.net/gh/xd003/goIndex-theme-nexmoe@master/js/flv.min.js"></script>'
+  '<script src="//cdn.jsdelivr.net/gh/xd003/goIndex-theme-nexmoe@master/js/flv.min.js"></script async>'
 );
 document.write(
-  '<script src="//cdn.jsdelivr.net/gh/xd003/goIndex-theme-nexmoe@master/js/DPlayer.min.js"></script>'
+  '<script src="//cdn.jsdelivr.net/gh/xd003/goIndex-theme-nexmoe@master/js/DPlayer.min.js"></script async>'
 );
 document.write(
-  '<script src="//cdn.jsdelivr.net/gh/xd003/goIndex-theme-nexmoe@master/js/markdown-it.min.js"></script>'
+  '<script src="//cdn.jsdelivr.net/gh/xd003/goIndex-theme-nexmoe@master/js/markdown-it.min.js"></script async>'
 );
 document.write(
   "<style>.mdui-appbar .mdui-toolbar{height:56px;font-size:1pc}.mdui-toolbar>i{opacity:.5}.mdui-toolbar>i{padding:0}.mdui-toolbar>a:hover,a.active,a.mdui-typo-headline{opacity:1}.mdui-container{max-width:980px}.mdui-list-item{transition:none}.mdui-list>.th{background-color:initial}.mdui-list-item>a{width:100%;line-height:3pc}.mdui-list-item{margin:2px 0;padding:0}.mdui-toolbar>a:last-child{opacity:1}@media screen and (max-width:980px){.mdui-list-item .mdui-text-right{display:none}.mdui-container{width:100%!important;margin:0}}</style>"
 );
 if (UI.theme == "material-dark") {
   document.write(
-    `<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/xd003/goIndex-theme-nexmoe@dev/css/material-dark.min.css">
+    `<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/xd003/goIndex-theme-nexmoe@master/css/material-dark.min.css" async>
     <meta name="theme-color" content="#121212">`
   );
 } else if (UI.theme == "dark") {
   document.write(
-    `<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/xd003/goIndex-theme-nexmoe@dev/css/dark.min.css">
+    `<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/xd003/goIndex-theme-nexmoe@master/css/dark.min.css" async>
     <meta name="theme-color" content="#0D0D0D">`
   );
 }
