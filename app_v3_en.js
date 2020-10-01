52140ad819ec2bdb385ddef5a2bb4d1f6cae5bda
Genos2000 <paraskamble2000@gmail.com>
2020-10-01 13:31:40 +0530

    added 2 dark themes, dark and material-dark

--- app_v3_en.js
+++ app_v3_en.js
@@ -5499,9 +5499,13 @@ document.write(
 document.write(
   "<style>.mdui-appbar .mdui-toolbar{height:56px;font-size:1pc}.mdui-toolbar>i{opacity:.5}.mdui-toolbar>i{padding:0}.mdui-toolbar>a:hover,a.active,a.mdui-typo-headline{opacity:1}.mdui-container{max-width:980px}.mdui-list-item{transition:none}.mdui-list>.th{background-color:initial}.mdui-list-item>a{width:100%;line-height:3pc}.mdui-list-item{margin:2px 0;padding:0}.mdui-toolbar>a:last-child{opacity:1}@media screen and (max-width:980px){.mdui-list-item .mdui-text-right{display:none}.mdui-container{width:100%!important;margin:0}}</style>"
 );
-if (UI.dark_mode) {
+if (UI.theme == "material-dark") {
   document.write(
-    `<style>* {box-sizing: border-box}body{color:rgba(255,255,255,.87);background-color:#333232}.mdui-theme-primary-${UI.main_color} .mdui-color-theme{background-color:#232427!important}</style>`
+    '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/xd003/goIndex-theme-nexmoe@master/css/material-dark.css">'
+  );
+} else if (UI.theme == "dark") {
+  document.write(
+    '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/xd003/goIndex-theme-nexmoe@master/css/material-dark.css">'
   );
 }
 function init() {
@@ -5675,7 +5679,7 @@ function list(path) {
 	 <div class="mdui-row"> 
 	  <ul id="list" class="mdui-list"> 
 	  </ul> 
-	  <div id="count" class="mdui-hidden mdui-center mdui-text-center mdui-m-b-3 mdui-typo-subheading mdui-text-color-blue-grey-500">Total <span class="number"></span> Item</div>
+	  <div id="count" class="mdui-hidden mdui-center mdui-text-center mdui-m-b-3 mdui-typo-subheading mdui-text-color-theme-200">Total <span class="number"></span> Item</div>
 	 </div>
 	`;
   $("#content").html(content);
@@ -5859,7 +5863,7 @@ function render_search_result_list() {
 	 <div class="mdui-row"> 
 	  <ul id="list" class="mdui-list"> 
 	  </ul> 
-	  <div id="count" class="mdui-hidden mdui-center mdui-text-center mdui-m-b-3 mdui-typo-subheading mdui-text-color-blue-grey-500">Total <span class="number"></span> Item(s)</div>
+	  <div id="count" class="mdui-hidden mdui-center mdui-text-center mdui-m-b-3 mdui-typo-subheading mdui-text-color-theme-200">Total <span class="number"></span> Item(s)</div>
 	 </div>
 	 <div id="readme_md" class="mdui-typo" style="display:none; padding: 20px 0;"></div>
 	`;
--- index.js
+++ index.js
@@ -41,9 +41,8 @@ const authConfig = {
  * web ui 设置
  */
 const uiConfig = {
-  "theme": "material", // DO NOT set it to classic
-  "dark_mode": false,
-  "main_color": "blue-grey",
+  "theme": "default", //default, material-dark, dark 
+  "main_color": "blue",
   "accent_color": "blue",
   "fluid_navigation_bar": true,
   "avatar": "https://i.ibb.co/DwMwdFv/photo-2020-05-25-22-58-28.jpg",
